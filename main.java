cce1e2da5801a45cedea9a92f2d6d0083ec0b05b
prabandanu76 <prabandanu76@gmail.com>
2024-11-28 14:55:20 +0700

    add new code

--- main.java
+++ main.java
@@ -2,5 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        String[] books = {"Java Programming", "Database Design", "Web Development"};
+
+        System.out.println("Available Books:"); // Perbaiki 'system' menjadi 'System'
+
+        for (String book : books) {
+            System.out.println("- " + book); // Perbaiki 'system' menjadi 'System'
+        }
     }
 }
